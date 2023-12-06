eeb839547d36d50f5f878f8e689b5e798f9ee0c2
EVG <olpol.ozoro@gmail.com>
2023-12-06 22:59:46 +0300

    update + fix

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -1,61 +1,59 @@
 from django.contrib.auth import get_user_model
 from django.db import models
-from django.utils.translation import gettext_lazy as _
 
 User = get_user_model()
 
 
 class BaseModel(models.Model):
     is_published = models.BooleanField(default=True,
-                                       verbose_name=_('Опубликовано'),
-                                       help_text=_('Снимите галочку, '
-                                                   'чтобы скрыть публикацию.'))
+                                       verbose_name='Опубликовано',
+                                       help_text='Снимите галочку, '
+                                                 'чтобы скрыть публикацию.')
     created_at = models.DateTimeField(auto_now_add=True,
-                                      verbose_name=_('Добавлено'))
+                                      verbose_name='Добавлено')
 
     class Meta:
         abstract = True
 
 
 class Category(BaseModel):
-    title = models.CharField(max_length=256, verbose_name=_('Заголовок'))
-    description = models.TextField(verbose_name=_('Описание'))
+    title = models.CharField(max_length=256, verbose_name='Заголовок')
+    description = models.TextField(verbose_name='Описание')
     slug = models.SlugField(unique=True,
-                            verbose_name=_('Идентификатор'),
-                            help_text=_(
-                                'Идентификатор страницы для URL; '
-                                'разрешены символы латиницы, '
-                                'цифры, дефис и подчёркивание.'))
+                            verbose_name='Идентификатор',
+                            help_text='Идентификатор страницы для URL; '
+                            'разрешены символы латиницы, '
+                            'цифры, дефис и подчёркивание.')
 
     class Meta:
-        verbose_name = _('категория')
-        verbose_name_plural = _('Категории')
+        verbose_name = 'категория'
+        verbose_name_plural = 'Категории'
 
 
 class Location(BaseModel):
-    name = models.CharField(max_length=256, verbose_name=_('Название места'))
+    name = models.CharField(max_length=256, verbose_name='Название места')
 
     class Meta:
-        verbose_name = _('местоположение')
-        verbose_name_plural = _('Местоположения')
+        verbose_name = 'местоположение'
+        verbose_name_plural = 'Местоположения'
 
 
 class Post(BaseModel):
-    title = models.CharField(max_length=256, verbose_name=_('Заголовок'))
-    text = models.TextField(verbose_name=_('Текст'))
-    pub_date = models.DateTimeField(verbose_name=_('Дата и время публикации'),
-                                    help_text=_(
-        'Если установить дату и время в будущем — '
-        'можно делать отложенные публикации.'))
+    title = models.CharField(max_length=256, verbose_name='Заголовок')
+    text = models.TextField(verbose_name='Текст')
+    pub_date = models.DateTimeField(verbose_name='Дата и время публикации',
+                                    help_text='Если установить дату и время '
+                                              'в будущем'
+                                    ' — можно делать отложенные публикации.')
     author = models.ForeignKey(User, on_delete=models.CASCADE,
-                               verbose_name=_('Автор публикации'))
+                               verbose_name='Автор публикации')
     location = models.ForeignKey(Location, on_delete=models.SET_NULL,
                                  null=True,
-                                 verbose_name=_('Местоположение'))
+                                 verbose_name='Местоположение')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL,
                                  null=True,
-                                 verbose_name=_('Категория'))
+                                 verbose_name='Категория')
 
     class Meta:
-        verbose_name = _('публикация')
-        verbose_name_plural = _('Публикации')
+        verbose_name = 'публикация'
+        verbose_name_plural = 'Публикации'
